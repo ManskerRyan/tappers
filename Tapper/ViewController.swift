49ec6e0d05e8f49967dca7d8a023628e9e56dfef
Ryan Alexander Mansker <Ryan@Ryans-MacBook-Pro.local>
2016-03-18 14:25:50 +0000

    Basic func for playBtn added

--- Tapper/ViewController.swift
+++ Tapper/ViewController.swift
@@ -9,17 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    //MARK: Outlets
+    @IBOutlet weak var logoImg: UIImageView!
+    @IBOutlet weak var howManyTapsTxt: UITextField!
+    @IBOutlet weak var playBtn: UIButton!
+    
+    @IBOutlet weak var tapBtn: UIButton!
+    @IBOutlet weak var tapsLbl: UILabel!
+    
+    
+    //MARK: Properties
+    var maxTaps: Int = 0
+    var currentTaps: Int = 0
+    
+    
+    //MARK: Functions
+    @IBAction func onPlayButtonPressed(sender: UIButton!) {
+        if howManyTapsTxt.text != nil && howManyTapsTxt.text != "" {
+            maxTaps = Int(howManyTapsTxt.text!)!
+            currentTaps = 0
+            
+            logoImg.hidden = true
+            howManyTapsTxt.hidden = true
+            playBtn.hidden = true
+            
+            tapBtn.hidden = false
+            tapsLbl.hidden = false
+            
+            tapsLbl.text = "\(currentTaps) Taps!"
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
+    
 }
 
